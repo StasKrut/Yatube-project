ab574c1c26baeb3c476a054bebb598b1a1a72b6b
Станислав Крутских <krutskih@inbox.ru>
2022-07-02 11:31:18 +0300

    Правки после 2го ревью

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client, override_settings
 
-from ..models import Post, Group, Comment
+from ..models import Post, Group
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
